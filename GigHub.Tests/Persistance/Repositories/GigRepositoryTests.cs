9bfd46773cfe8bb322aeb71f3a82f7fcc4bfdea2
Ivica Mamic <mamic.ivica@gmail.com>
2017-10-29 11:49:44 +0100

    UnitTests v2

--- GigHub.Tests/Persistance/Repositories/GigRepositoryTests.cs
+++ GigHub.Tests/Persistance/Repositories/GigRepositoryTests.cs
@@ -17,15 +17,20 @@ namespace GigHub.Tests.Persistance.Repositories
     {
         private GigRepository _repository;
         private Mock<DbSet<Gig>> _mockGigs;
+        private Mock<DbSet<Attendance>> _mockAttendances;
+        private string _userId;
 
         [TestInitialize]
         public void TestInitialize()
         {
             _mockGigs = new Mock<DbSet<Gig>>();
+            _mockAttendances = new Mock<DbSet<Attendance>>();
 
             var mockContext = new Mock<IApplicationDbContext>();
             mockContext.SetupGet(c => c.Gigs).Returns(_mockGigs.Object);
+            mockContext.SetupGet(c => c.Attendances).Returns(_mockAttendances.Object);
 
+            _userId = "1";
             _repository = new GigRepository(mockContext.Object);
         }
 
@@ -35,12 +40,12 @@ namespace GigHub.Tests.Persistance.Repositories
             var gig = new Gig()
             {
                 DateTime = DateTime.Now.AddDays(-1),
-                ArtistId = "1"
+                ArtistId = _userId
             };
 
             _mockGigs.SetSource(new[] { gig });
 
-            var gigs = _repository.GetUsersActiveFutureGigsWithGenre("1");
+            var gigs = _repository.GetUsersActiveFutureGigsWithGenre(_userId);
 
             gigs.Should().BeEmpty();
         }
@@ -51,15 +56,105 @@ namespace GigHub.Tests.Persistance.Repositories
             var gig = new Gig()
             {
                 DateTime = DateTime.Now.AddDays(1),
-                ArtistId = "1"
+                ArtistId = _userId
             };
             gig.Cancel();
 
             _mockGigs.SetSource(new[] { gig });
 
-            var gigs = _repository.GetUsersActiveFutureGigsWithGenre("1");
+            var gigs = _repository.GetUsersActiveFutureGigsWithGenre(_userId);
 
             gigs.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetUpcomingGigsByArtist_GigIsForADifferentArtist_ShouldNotBeReturned()
+        {
+            var gig = new Gig()
+            {
+                DateTime = DateTime.Now.AddDays(1),
+                ArtistId = _userId
+            };
+
+            _mockGigs.SetSource(new[] { gig });
+
+            var gigs = _repository.GetUsersActiveFutureGigsWithGenre(gig.ArtistId + "-");
+
+            gigs.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetUpcomingGigsByArtist_GigIsForTheGvenArtinsAndInFuture_ShouldBeReturned()
+        {
+            var gig = new Gig()
+            {
+                DateTime = DateTime.Now.AddDays(1),
+                ArtistId = _userId
+            };
+
+            _mockGigs.SetSource(new[] { gig });
+
+            var gigs = _repository.GetUsersActiveFutureGigsWithGenre(gig.ArtistId);
+
+            gigs.Should().Contain(gig);
+        }
+
+        [TestMethod]
+        public void GetGigsUserAttending_GigIsInThePast_ShouldNotBeReturned()
+        {
+            var gig = new Gig()
+            {
+                DateTime = DateTime.Now.AddDays(-1)
+            };
+            var attendance = new Attendance()
+            {
+                Gig = gig,
+                AttendeeId = _userId
+            };
+
+            _mockAttendances.SetSource(new[] {attendance});
+
+            var gigs = _repository.GetGigsUserAttending(attendance.AttendeeId);
+
+            gigs.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetGigsUserAttending_AttendanceForADifferentUser_ShouldNotBeReturned()
+        {
+            var gig = new Gig()
+            {
+                DateTime = DateTime.Now.AddDays(1)
+            };
+            var attendance = new Attendance
+            {
+                Gig = gig, AttendeeId = _userId
+            };
+
+            _mockAttendances.SetSource(new[] { attendance });
+
+            var gigs = _repository.GetGigsUserAttending(attendance.AttendeeId + "-");
+
+            gigs.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetGigsUserAttending_UpcomingGigUserAttending_ShouldBeReturned()
+        {
+            var gig = new Gig()
+            {
+                DateTime = DateTime.Now.AddDays(1)
+            };
+            var attendance = new Attendance
+            {
+                Gig = gig, AttendeeId = _userId
+            };
+
+            _mockAttendances.SetSource(new[] { attendance });
+
+            var gigs = _repository.GetGigsUserAttending(attendance.AttendeeId);
+
+            gigs.Should().Contain(gig);
         }
     }
 }
---
+++ GigHub.Tests/Persistance/Repositories/NotificationsRepositoryTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using FluentAssertions;
+using GigHub.Core.Models;
+using GigHub.Persistence;
+using GigHub.Persistence.Repositories;
+using GigHub.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data.Entity;
+
+namespace GigHub.Tests.Persistance.Repositories
+{
+    [TestClass]
+    public class NotificationsRepositoryTests
+    {
+        private NotificationsRepository _repository;
+        private Mock<DbSet<UserNotification>> _mockNotifications;
+        private string _userId;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockNotifications = new Mock<DbSet<UserNotification>>();
+
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.UserNotifications).Returns(_mockNotifications.Object);
+
+
+            _repository = new NotificationsRepository(mockContext.Object);
+        }
+
+        [TestMethod]
+        public void GetNewNotificationsFor_NotificationIsRead_ShouldNotBeReturned()
+        {
+            var notification = Notification.GigCanceled(new Gig());
+            var user = new ApplicationUser { Id = _userId };
+            var userNotification = new UserNotification(user, notification);
+            userNotification.Read();
+
+            _mockNotifications.SetSource(new[] { userNotification });
+
+            var notifications = _repository.GetNewNotifications(user.Id);
+
+            notifications.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetNewNotification_NotificationIsForADifferentUser_ShouldNotBeReturned()
+        {
+            var notification = Notification.GigCanceled(new Gig());
+            var user = new ApplicationUser { Id = _userId };
+            var userNotification = new UserNotification(user, notification);
+
+            _mockNotifications.SetSource(new[] { userNotification });
+
+            var notifications = _repository.GetNewNotifications(user.Id + "-");
+
+            notifications.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetNewNotificationsFor_NewNotificationForTheGivenUser_ShouldBeReturned()
+        {
+            var notification = Notification.GigCanceled(new Gig());
+            var user = new ApplicationUser { Id = _userId };
+            var userNotification = new UserNotification(user, notification);
+
+            _mockNotifications.SetSource(new[] { userNotification });
+
+            var notifications = _repository.GetNewNotifications(user.Id);
+
+            notifications.Should().HaveCount(1);
+            notifications.First().Should().Be(notification);
+        }
+    }
+}
